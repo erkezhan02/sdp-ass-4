330f79859579aa6c69ad060f9d7b61eb226bd092
aweoxeoz <154399862+erkezhan02@users.noreply.github.com>
2024-10-14 18:15:08 +0500

    Update IteratorDemo.java

--- IteratorDemo.java
+++ IteratorDemo.java
@@ -1,15 +1,11 @@
-   private class NameIterator implements Iterator {
-        private int index;
-
-        public boolean hasNext() {
-            return index < names.length;
-        }
-
-        public Object next() {
-            if (this.hasNext()) {
-                return names[index++];
-            }
-            return null;
+public class IteratorDemo {
+    public static void main(String[] args) {
+        NameRepository nameRepository = new NameRepository();
+        Iterator iterator = nameRepository.getIterator();
+        
+        System.out.println("Iterating through names:");
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
         }
     }
 }
