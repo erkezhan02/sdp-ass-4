3298b38128c73e3033714896284a16217c045380
aweoxeoz <154399862+erkezhan02@users.noreply.github.com>
2024-10-14 18:14:37 +0500

    Create IteratorDemo.java

---
+++ IteratorDemo.java
@@ -0,0 +1,15 @@
+   private class NameIterator implements Iterator {
+        private int index;
+
+        public boolean hasNext() {
+            return index < names.length;
+        }
+
+        public Object next() {
+            if (this.hasNext()) {
+                return names[index++];
+            }
+            return null;
+        }
+    }
+}
